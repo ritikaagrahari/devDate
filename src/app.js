eef5a1bf68f1968fb0edddb65e6ff5ac43b38353
ritikaagrahari <ritikaagrahari2002@gmail.com>
2025-03-19 12:37:02 +0530

    writing middleware

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 const express = require("express");
 
 const app = express();
-
+const { adminAuth, userAuth } = require("./middlewares/auth");
 // Request handler
-app.use("/", (req, res) => {
-  res.send("Hello from dashboard");
+app.use("/admin", adminAuth);
+app.use("/user/data", userAuth, (req, res) => {
+  res.send("User data sent");
 });
-app.use("/hello", (req, res) => {
-  res.send("Hello hello");
+app.use("/admin/getData", (req, res) => {
+  res.send("Data sent");
 });
-app.use("/test", (req, res) => {
-  res.send("Hello Test");
+app.use("/admin/deleteUser", (req, res) => {
+  res.send("Deleted the user");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000...");
+app.listen(7777, () => {
+  console.log("Server is running on port 7777...");
 });
