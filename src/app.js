c93e300f1755cfbc85f6b2c121daf4607871905f
ritikaagrahari <ritikaagrahari2002@gmail.com>
2025-05-25 18:18:49 +0530

    API level data sanitization

--- src/app.js
+++ src/app.js
@@ -54,17 +54,28 @@ app.delete("/user", async (req, res) => {
 });
 
 // update user api (Update data of the user)
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
   try {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update not Allowed");
+    }
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cannot be greater than 10");
+    }
     const user = await User.findByIdAndUpdate({ _id: userId }, data, {
       returnDocument: "after",
+      runValidators: "true",
     });
-    console.log(user);
+    // console.log(user);
     res.send("User Updated Successfully");
   } catch (err) {
-    res.status(400).send("Something Went Wrong");
+    res.status(400).send("Update Failed: " + err.message);
   }
 });
 
